3f364b1e4621001f8bedb9ad8482784b3a9b5698
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-01-12 13:26:51 +0100

    Add fetch quote function

---
+++ src/functions.js
@@ -0,0 +1,36 @@
+//*let soundElementCat = document.getElementById("catsound");
+async function getFact() {
+  const API_URL =
+    "https://uselessfacts.jsph.pl/api/v2/facts/random?language=en";
+  try {
+    const response = await fetch(API_URL);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+
+    let randomFact = json.text;
+    showRandomFact(randomFact);
+    console.log(randomFact);
+
+    localStorage.setItem("quote", JSON.stringify(randomFact));
+
+    //const favQuote = localStorage.getItem("quote");
+
+    const savedFact = JSON.parse(localStorage.getItem("quote"));
+    console.log(savedFact);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+getFact();
+
+function showRandomFact(randomFact) {
+  let random = document.querySelector("#text");
+  random.textContent = randomFact;
+}
+
+let button = document.querySelector("#changeFact");
+button.addEventListener("click", getFact);
